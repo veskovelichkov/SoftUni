aab0d6ab10bbf3f1479c3c96d25990c3fa550f31
veskovelichkov <veselin.velichkov00@gmail.com>
2023-02-13 16:03:32 +0100

    Solved tasks from list basics

--- Python_Fundamentals/list_basics/easter_gifts.py
+++ Python_Fundamentals/list_basics/easter_gifts.py
@@ -1,17 +1,33 @@
-gifts_string=input()
-gifts_list=gifts_string.split(" ")
-command=input()
-while command!='No Money':
-    command_list=command.split(" ")
-    if len(command_list)==2:
-        if command_list[0]=="OutOfStock":
-            gifts_list=[w.replace(command_list[1],"None") for w in gifts_list]
-        elif command_list[0]=="JustInCase":
-            gifts_list[-1]=command_list[1]
-    elif len(command_list)==3:
-        if command_list[0]=="Required":
-            index=int(command_list[-1])
-            gifts_list[index-1]=command_list[1]
-    command=input()
-gifts_list_updated=[v for v in gifts_list if v!="None"]
-print(" ".join(gifts_list_updated))
+def outofstock(sequence, chosen_gift):
+    sequence = list(map(lambda x: x.replace(chosen_gift, "None"), sequence))
+    return sequence
+
+
+def required(sequence, chosen_gift, chosen_index):
+    if chosen_index in range(len(sequence)):
+        sequence[chosen_index] = chosen_gift
+    return sequence
+
+
+def justincase(sequence, chosen_gift):
+    sequence[-1] = chosen_gift
+    return sequence
+
+
+gifts = list(map(str, input().split(" ")))
+while True:
+    command = input()
+    if command == "No Money":
+        break
+    command_split = command.split(" ")
+    event = command_split[0]
+    gift = command_split[1]
+    if event == "OutOfStock":
+        gifts = outofstock(gifts, gift)
+    elif event == "Required":
+        index = int(command_split[2])
+        gifts = required(gifts, gift, index)
+    elif event == "JustInCase":
+        gifts = justincase(gifts, gift)
+gifts=[value for value in gifts if value!="None"]
+print(" ".join(gifts))
--- Python_Fundamentals/list_basics/tic_tac_toe.py
+++ Python_Fundamentals/list_basics/tic_tac_toe.py
@@ -1,9 +1,42 @@
-first_line=input().split(" ")
-second_line=input().split(" ")
-third_line=input().split(" ")
+first_line = list(map(int, input().split(" ")))
+second_line = list(map(int, input().split(" ")))
+third_line = list(map(int, input().split(" ")))
+first_player = False
+second_player = False
+if len(set(first_line)) == 1 or len((set(second_line))) == 1 or len((set(third_line))) == 1:
+    if first_line[0] == 1:
+        first_player = True
+    elif first_line[0] == 2:
+        second_player = True
+if len((set(second_line))) == 1:
+    if second_line[0] == 1:
+        first_player = True
+    elif second_line[0] == 2:
+        second_player = True
+if len((set(third_line))) == 1:
+    if third_line[0] == 1:
+        first_player = True
+    elif third_line[0] == 2:
+        second_player = True
 for i in range(3):
-    if first_line[i] == second_line[i]== third_line[i]:
-        if first_line[i] == "1":
-            print("First player won!")
-        elif first_line[i] == "2":
-            print("Second player won!")
+    if first_line[i] == second_line[i] == third_line[i]:
+        if first_line[0] == 1:
+            first_player = True
+        elif first_line[0] == 2:
+            second_player = True
+if first_line[0] == second_line[1] == third_line[2]:
+    if first_line[0] == 1:
+        first_player = True
+    elif first_line[0] == 2:
+        second_player = True
+if first_line[2] == second_line[1] == third_line[0]:
+    if first_line[2] == 1:
+        first_player = True
+    elif first_line[2] == 2:
+        second_player = True
+if first_player:
+    print("First player won")
+elif second_player:
+    print("Second player won")
+elif not first_player and not second_player:
+    print("Draw!")
